208d384aa6d6d9658e0f43e22d4ee3f7841188ca
ayher <1781535740@qq.com>
2020-03-05 18:26:23 +0800

    Add route

---
+++ src/Page.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import App from './App.js'
+import { HashRouter , Route  } from 'react-router-dom';
+
+export default  () => {
+	return(
+			<HashRouter>
+				<Route path="/home" component={App} />
+			</HashRouter>
+		)
+}
